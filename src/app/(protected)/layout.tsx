e1e1ae31d4d74e54548cff7a7d89a0979639b9f8
manishmh <manishmh982@gmail.com>
2025-05-18 07:50:40 +0530

    sessionguard

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,9 +2,9 @@ import SessionGuard from "@/components/global/session-guard";
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    // <SessionGuard>
-    <>{children}</>
-    // </SessionGuard>
+    <SessionGuard>
+      <>{children}</>
+    </SessionGuard>
   );
 };
 
