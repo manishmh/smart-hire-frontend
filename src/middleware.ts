3c92f8bd8bf0ed28cf602618813c8451e32c706c
manishmh <manishmh982@gmail.com>
2025-05-26 07:19:57 +0530

    middleware added and removed token checkn in /auth/login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,15 @@
-import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-} 
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  if (!accessToken && request.nextUrl.pathname !== "/auth/login") {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/dashboard/:path*"], 
