75959db5e6d42838adc3b824f18820a5030eb498
manishmh <manishmh982@gmail.com>
2025-05-26 19:47:07 +0530

    removed logic of middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const accessToken = request.cookies.get("accessToken")?.value;
+  // const accessToken = request.cookies.get("accessToken")?.value;
 
-  if (!accessToken && request.nextUrl.pathname !== "/dashboard") {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
+  // if (!accessToken && request.nextUrl.pathname !== "/dashboard") {
+  //   return NextResponse.redirect(new URL("/auth/login", request.url));
+  // }
 
   return NextResponse.next();
 }
